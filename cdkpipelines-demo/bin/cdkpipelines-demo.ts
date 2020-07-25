e4176746eec113c5603823981218dba86ca196ca
Sadayoshi Tada <tadainfra@gmail.com>
2020-07-25 12:25:21 +0900

    Initial deployment of our empty pipeline

--- cdkpipelines-demo/bin/cdkpipelines-demo.ts
+++ cdkpipelines-demo/bin/cdkpipelines-demo.ts
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { CdkpipelinesDemoStack } from '../lib/cdkpipelines-demo-stack';
+import { App } from '@aws-cdk/core';
+import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
 
-const app = new cdk.App();
-new CdkpipelinesDemoStack(app, 'CdkpipelinesDemoStack');
+const app = new App();
+
+new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
+  env: { account: '506364634079', region: 'ap-northeast-1' },
+});
+
+app.synth();
